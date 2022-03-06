0a44619d992039f234dda0c76032f26ff09f3060
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-03-06 09:52:15 -0300

    Menu toggle

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-/* / */
+/* Menu-btn/Botão de menu */
+let menu = document.querySelector('#menu-btn');
+let header = document.querySelector('.header');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
