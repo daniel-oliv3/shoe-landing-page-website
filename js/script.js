e22ef84e1c9a77ac3a8824c2a923429ac01b5a17
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-03-06 10:14:25 -0300

    Responsive

--- js/script.js
+++ js/script.js
@@ -5,4 +5,14 @@ let header = document.querySelector('.header');
 menu.onclick = () => {
     menu.classList.toggle('fa-times');
     header.classList.toggle('active');
+    document.body.classList.toggle('active');
+}
+
+/**/
+window.onscroll = () => {
+    if(window.innerWidth < 1200){
+        menu.classList.remove('fa-times');
+        header.classList.remove('active');
+        document.body.classList.remove('active');
+    }
 }
